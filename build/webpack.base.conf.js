3a4773a8d0dacfdfd4ac5f925582c80ccd884095
gipnocode <tatjana.voroni@yandex.ru>
2020-03-01 05:58:17 +0300

    Add all pages to dist

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,12 +8,29 @@ const PATHS = {
   src: path.join(__dirname, '../src'),
   dist: path.join(__dirname, '../dist'),
   assets: 'assets/'
+
 };
 
 
-const PAGES_DIR = `${PATHS.src}/pages/landing/`;
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+// const PAGES_DIR = path.resolve(__dirname, '../src/pages');
+// const PAGES = fs.readdirSync(PAGES_DIR);
+const PAGES_DIR1 = `${PATHS.src}/pages/index`;
+const PAGES1 = fs.readdirSync(PAGES_DIR1).filter(fileName => fileName.endsWith('.pug'));
 
+const PAGES_DIR2 = `${PATHS.src}/pages/landing`;
+const PAGES2 = fs.readdirSync(PAGES_DIR2).filter(fileName => fileName.endsWith('.pug'));
+//
+const PAGES_DIR3 = `${PATHS.src}/pages/registration`;
+const PAGES3 = fs.readdirSync(PAGES_DIR3).filter(fileName => fileName.endsWith('.pug'));
+//
+const PAGES_DIR4 = `${PATHS.src}/pages/signin`;
+const PAGES4 = fs.readdirSync(PAGES_DIR4).filter(fileName => fileName.endsWith('.pug'));
+
+const PAGES_DIR5 = `${PATHS.src}/pages/room-details`;
+const PAGES5 = fs.readdirSync(PAGES_DIR5).filter(fileName => fileName.endsWith('.pug'));
+
+const PAGES_DIR6 = `${PATHS.src}/pages/ui`;
+const PAGES6 = fs.readdirSync(PAGES_DIR6).filter(fileName => fileName.endsWith('.pug'));
 module.exports = {
 
   externals: {
@@ -26,13 +43,13 @@ module.exports = {
   output: {
     filename: `[name].[hash].js`,
     path: PATHS.dist,
-    publicPath: '/'
+
   },
 
   module: {
     rules: [{
       test: /\.pug$/,
-      loader: 'pug-loader'
+      loader: 'pug-loader',
 
     },
       {
@@ -49,13 +66,7 @@ module.exports = {
       }
     },
 
-      {
-      test: /\.(png|jpg|gif|svg|ico)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    },
+
       {
       test: /\.sass$/,
       use: [
@@ -97,20 +108,43 @@ module.exports = {
     }
   },
   plugins: [
-
+    // [new StylelintPlugin(options)],
+    new PrettierPlugin(),
     new MiniCssExtractPlugin({
       filename: `[name].[hash].css`,
     }),
+    // new CleanWebpackPlugin(),
+
     new CopyWebpackPlugin([
-      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+      // { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
       { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
       { from: `${PATHS.src}/static`, to: '' },
     ]),
 
 
-    ...PAGES.map(page => new HtmlWebpackPlugin({
-      template: `${PAGES_DIR}/${page}`,
+    ...PAGES1.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR1}/${page}`,
       filename: `./${page.replace(/\.pug/,'.html')}`
-    }))
+    })),
+    ...PAGES2.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR2}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
+    ...PAGES3.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR3}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
+    ...PAGES4.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR4}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
+    ...PAGES5.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR5}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
+    ...PAGES6.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR6}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    })),
   ],
 };

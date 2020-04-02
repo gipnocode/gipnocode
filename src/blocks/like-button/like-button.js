7fdf7a263f77e1e80dfe3d18815f6e798e115fa1
gipnocode <tatjana.voroni@yandex.ru>
2020-04-02 08:50:42 +0300

    Averge dizaster
    Working on toggle, radio buttons, registration form

--- src/blocks/like-button/like-button.js
+++ src/blocks/like-button/like-button.js
@@ -1,10 +1,15 @@
-$(function() {
-  $(document).one("click", ".like-review", function(e) {
-    $(this).html(
-      '<i class="fa fa-heart" aria-hidden="true"></i> You liked this'
-    );
-    $(this)
-      .children(".fa-heart")
-      .addClass("animate-like");
-  });
+$(".like__btn").on("click", function() {
+  // Check if it's already been clicked
+  if (!$(this).hasClass("like__btn--disabled")) {
+    // Update the number
+    updated_likes = parseInt($(".like__btn span").html()) + 1;
+    $(".like__btn span").html(updated_likes);
+  }
+  // Make btn disabled
+  $(this)
+    .attr("disabled", true)
+    .addClass("tada");
+});
+$(".like-button").on("click", function() {
+  $(this).toggleClass("violet");
 });

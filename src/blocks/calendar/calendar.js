17ba1dc31aa582b7b358e9d82b92ce65deabdcec
tanja <tatjana.voroni2015@yandex.ru>
2020-04-21 23:04:42 +0300

    Calendar refactor

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,15 +1,35 @@
 $(function () {
   $(".mytest").dateRangePicker({
+    language: "ru",
+    singleMonth: true,
+    customArrowNextSymbol: '<i class="material-icons">arrow_forward</i>',
+    customArrowPrevSymbol: '<i class="material-icons">arrow_back</i>',
+    stickyMonths: true,
+    startDate: "2019-07-10",
+    endDate: "2019-09-10",
+    startOfWeek: "monday",
+    alwaysOpen: true,
+    showTopbar: false,
+  });
+
+  $(".mytest2").dateRangePicker({
     separator: " to ",
 
     getValue: function () {
-      if ($(".mytest1").val() && $(".mytest2").val())
-        return $(".mytest1").val() + " to " + $(".mytest2").val();
+      if (
+        $(".input-field_calendar-one").val() &&
+        $(".input-field_calendar-two").val()
+      )
+        return (
+          $(".input-field_calendar-one").val() +
+          " to " +
+          $(".input-field_calendar-two").val()
+        );
       else return "";
     },
     setValue: function (s, s1, s2) {
-      $(".mytest1").val(s1);
-      $(".mytest2").val(s2);
+      $(".input-field_calendar-one").val(s1);
+      $(".input-field_calendar-two").val(s2);
     },
 
     getValue: function () {
@@ -22,12 +42,7 @@ $(function () {
     singleMonth: true,
     customArrowNextSymbol: '<i class="material-icons">arrow_forward</i>',
     customArrowPrevSymbol: '<i class="material-icons">arrow_back</i>',
-
-    stickyMonths: true,
-    startDate: "2019-07-10",
-    endDate: "2019-09-10",
     startOfWeek: "monday",
-    alwaysOpen: true,
     showTopbar: false,
   });
   $("#date-range16-close").click(function (evt) {
